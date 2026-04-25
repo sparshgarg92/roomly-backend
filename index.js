126833309513d46af1b913b1d61e892355297894
Sparsh Garg <sparshgarg@Sparshs-MacBook-Air.local>
2026-04-25 13:55:34 -0700

    fix multer on vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({ origin: '*' }));
-app.use(express.json({ limit: '50mb' }));
+app.use((req, res, next) => { if (req.headers['content-type']?.includes('multipart/form-data')) return next(); express.json({ limit: '50mb' })(req, res, next); });
 
 app.use('/api', require('./routes/floorplan'));
 
